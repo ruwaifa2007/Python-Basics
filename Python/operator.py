619ec283d330515bf05604cb52f2e54190e99f52
Mohammad Ruwaifa <ruwaifa2007@gmail.com>
2026-04-22 21:42:55 +0530

    Create operator.py

---
+++ Python/operator.py
@@ -0,0 +1,14 @@
+num1=int(input("enter the value of no.= "))  # Taking input using input function
+num2=int(input("enter the value of no.= "))  # Taking input using input function
+add=num1+num2            # performing addition operation
+print("addition of",num1, "+",num2 ,"is",add)  #  Printing the addition of two no.
+sub=num1-num2            # performing subtraction operation
+print("subtraction of",num1,"-",num2,"is",sub)  #Printing the subtraction of two no.
+mul=num1*num2            # performing multiplication operation
+print("multiplication of",num1,"&",num2,"is",mul)  #Printing the multiplication of two no.
+div=num1/num2             # performing division operation
+print("division of ",num1,"&",num2,"is",div)  # Printing the division of two no.
+flo=num1//num2            # performing floor division operation
+print("floor division of",num1,"&",num2,"is",flo)    # Printing the floor division of two no.
+exp=num1**num2            # performing exponential operation
+print("exponential of",num1,"&",num2,"is",exp)  # Printing the exponential of two no.
