434412390140d9aaabacbb54b63ad473dc8126f0
Mohammad Ruwaifa <ruwaifa2007@gmail.com>
2026-04-22 20:56:16 +0530

    Create variable.py

---
+++ Python/variable.py
@@ -0,0 +1,12 @@
+a=10 # creating int varible 'a'
+print(a)
+b=True # creating boolean data type varible 'b'
+print(b)
+c="Ruwaifa" # creating a string data type varible 'c'
+print(c)
+d=1.23458 # creating a float data type varible 'd'
+print(d)
+print("this is a ",type(a))  # showing the data type of the varible 'a'
+print("this is a ",type(b))  # showing the data type of the varible 'b'
+print("this is a",type(c))   # showing the data type of the varible'c'
+print("this is a",type(d))   # showing the data type of the varible 'd'
