71443a7b79e692c3b26257b9b73b2be845c36f69
Mohammad Ruwaifa <ruwaifa2007@gmail.com>
2026-09-18 11:02:25 +0530

    Add files via upload

---
+++ Python/05 user input.py
@@ -0,0 +1,12 @@
+# Taking user input in python
+
+# Input Function
+
+name = input("Enter your name:") # here input() function is used to take input from the user
+
+print("My Name is", name) # displays the name entered by the user
+
+#type conversion
+
+age = int(input("Enter your Age:")) # here input() function is used to take input from the user and int() function is used to convert the input from string to integer
+print(age) # displays the age entered by the user
